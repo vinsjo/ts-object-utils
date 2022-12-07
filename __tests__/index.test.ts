9d9d6072f9342144098615b16a2b72c938d0c8e7
Vincent Sjögren <vincentsjogren@gmail.com>
2022-12-07 16:36:29 +0100

    updated type definitions and added pick and omit functions

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals';
-import { objectKeys, objectEntries, objectValues } from '../src';
+import { objectKeys, objectEntries, objectValues, pick, omit } from '../src';
 
 test('objectKeys', () => {
     expect(objectKeys(null)).toEqual([]);
@@ -25,4 +25,16 @@ test('objectValues', () => {
     expect(objectValues(null)).toEqual([]);
     expect(objectValues({ a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
     expect(objectValues([1, 2, 3])).toEqual([1, 2, 3]);
+});
+
+test('pick', () => {
+    expect(pick(null)).toEqual({});
+    expect(pick({ a: 1, b: 2, c: 3 }, 'a', 'c')).toEqual({ a: 1, c: 3 });
+    expect(pick({ a: 1, b: 2, c: 3 })).toEqual({});
+});
+
+test('omit', () => {
+    expect(omit(null)).toEqual({});
+    expect(omit({ a: 1, b: 2, c: 3 }, 'a', 'c')).toEqual({ b: 2 });
+    expect(omit({ a: 1, b: 2, c: 3 })).toEqual({ a: 1, b: 2, c: 3 });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,41 @@
-export function objectKeys<
-    K extends keyof T,
-    T = Record<string | number | symbol, unknown>
->(obj: T): K[] {
+export function objectKeys<T = Record<string | number | symbol, unknown>>(
+    obj: T
+): Array<keyof T> {
     if (!(obj instanceof Object)) return [];
-    return Object.keys(obj) as K[];
+    return Object.keys(obj) as Array<keyof T>;
 }
 
-export function objectEntries<
-    K extends keyof T,
-    T = Record<string | number | symbol, unknown>
->(obj: T): [K, T[K]][] {
+export function objectEntries<T = Record<string | number | symbol, unknown>>(
+    obj: T
+): Array<[keyof T, T[keyof T]]> {
     if (!(obj instanceof Object)) return [];
-    return Object.entries(obj) as [K, T[K]][];
+    return Object.entries(obj) as Array<[keyof T, T[keyof T]]>;
 }
 
-export function objectValues<
-    K extends keyof T,
-    T = Record<string | number | symbol, unknown>
->(obj: T): T[K][] {
+export function objectValues<T = Record<string | number | symbol, unknown>>(
+    obj: T
+): T[keyof T][] {
     if (!(obj instanceof Object)) return [];
-    return Object.values(obj) as T[K][];
+    return Object.values(obj) as T[keyof T][];
+}
+
+export function pick<T = Record<string | number | symbol, unknown>>(
+    obj: T,
+    ...keys: Array<keyof T>
+): Pick<T, typeof keys[number]> {
+    return objectEntries(obj)
+        .filter(([key]) => keys.includes(key))
+        .reduce((output, [key, value]) => {
+            return { ...output, [key]: value };
+        }, {}) as Pick<T, typeof keys[number]>;
+}
+export function omit<T = Record<string | number | symbol, unknown>>(
+    obj: T,
+    ...keys: Array<keyof T>
+): Omit<T, typeof keys[number]> {
+    return objectEntries(obj)
+        .filter(([key]) => !keys.includes(key))
+        .reduce((output, [key, value]) => {
+            return { ...output, [key]: value };
+        }, {}) as Pick<T, typeof keys[number]>;
 }
